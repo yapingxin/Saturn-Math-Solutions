19c70aa322cac13d5f277316aa530ea6e37abf93
Yaping Xin <xinyp@live.com>
2018-09-05 16:25:47 +0800

    refactor and remove iLOG3

--- Code/Utilities/Matrix-Integration/Matrix-CSV-Integration/Matrix-CSV-Func.c
+++ Code/Utilities/Matrix-Integration/Matrix-CSV-Integration/Matrix-CSV-Func.c
@@ -5,6 +5,9 @@
 #include <errno.h>
 
 
+static HResult check_in_out_folder(const char* in_folder, const char* out_folder);
+
+
 // Return values:
 //	    HResult_OK (1)                      | Success;
 //		HResult_PARAM_NULL (0x00110000)     | Any of the input parameters is NULL;
@@ -19,15 +22,41 @@ HResult matrix_add_csv(const char* in_folder, const char* out_folder)
     size_t path_len_0 = 0;
     size_t path_len_1 = 0;
 
-	/******************************/
+    rc = check_in_out_folder(in_folder, out_folder);
+    if (rc != HResult_OK)
+    {
+        goto EXIT;
+    }
+
+EXIT:
+	return rc;
+}
+
+
+
+// Return values:
+//	    HResult_OK (1)                      | Success;
+//		HResult_PARAM_NULL (0x00110000)     | Any of the input parameters is NULL;
+//                          0x00110001      | Any of the input parameters is empty;
+//                          0x00110002      | Any of the input parameters is too long (over 240 characters);
+//      HResult_FILE_NOTEXISTS (0x00130001) | Any of the input parameters is not exist as folder path.
+//      HResult_FILE_CannotRead (0x00130002)| The path of in_folder cannot be read.
+//      HResult_FILE_CannotWrite 0x00130004 | The path of out_folder cannot be written.
+static HResult check_in_out_folder(const char* in_folder, const char* out_folder)
+{
+    HResult rc = HResult_OK;
+    size_t path_len_0 = 0;
+    size_t path_len_1 = 0;
+
+    /******************************/
     /* Check folder path :: Begin */
     /******************************/
 
     if (in_folder == NULL || out_folder == NULL)
-	{
-		rc = HResult_PARAM_NULL;
-		goto EXIT;
-	}
+    {
+        rc = HResult_PARAM_NULL;
+        goto EXIT;
+    }
 
     path_len_0 = strlen(in_folder);
     path_len_1 = strlen(out_folder);
@@ -67,5 +96,5 @@ HResult matrix_add_csv(const char* in_folder, const char* out_folder)
     /******************************/
 
 EXIT:
-	return rc;
+    return rc;
 }
